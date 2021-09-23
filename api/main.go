d4f9eb5d2d7aa7bfdd384aa623ad8713cd723a9d
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-23 19:44:55 -0400

    Feat: Most of main.go finished

---
+++ api/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"hits/api/prisma/db"
+	. "hits/api/utils"
+	. "hits/api/v1"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(Response{
+			Success: true,
+			Message: "Welcome to Hits API!",
+		})
+	})
+
+	/* V1 */
+	v1 := app.Group("/v1")
+	v1.Get("/", Introduction)
+	v1.Get("/topHits", GetTopHits)
+	v1.Get("/hits/:url", GetHits)
+}
+
+func main() {
+	godotenv.Load()
+
+	app := fiber.New(fiber.Config{
+		CaseSensitive: false,
+		StrictRouting: true,
+		ServerHeader:  "HITS API",
+		AppName:       "HITS API v1.0",
+		BodyLimit:     1024 * 1024,
+	})
+
+	app.Use(logger.New(logger.Config{
+		Format: "${time} |   ${cyan}${status} ${reset}|   ${latency} | ${ip} on ${cyan}${ua} ${reset}| ${cyan}${method} ${reset}${path} \n",
+	}))
+
+	app.Use(recover.New(recover.Config{
+		Next:             nil,
+		EnableStackTrace: true,
+	}))
+
+	setupRoutes(app)
+
+	if GetPrisma() == nil {
+		SetGlobalDb(db.NewClient())
+	}
+
+	if err := GetPrisma().Prisma.Connect(); err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := GetPrisma().Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
+}
