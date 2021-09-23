7c8c66c5e56fb025b6c5d76bf4130857630de91b
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-23 19:55:56 -0400

    Feat: The entire functionality

--- api/v1/api.go
+++ api/v1/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"hits/api/prisma/db"
 	. "hits/api/utils"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -55,11 +56,41 @@ func GetHits(c *fiber.Ctx) error {
 }
 
 func GetTopHits(c *fiber.Ctx) error {
-	var take = c.Query("take")
-	println(take)
+	var reqTake = c.Query("take")
+	var client = GetPrisma()
+	var ctx = context.Background()
+
+	/* If requester didn't put take in request */
+	if len(reqTake) <= 0 {
+		reqTake = "0"
+	}
+
+	take, err := strconv.Atoi(reqTake)
+	if err != nil {
+		return c.Status(400).JSON(Response{
+			Success: false,
+			Message: "Your request is improper!",
+		})
+	}
+
+	if take > 10 || take <= 0 {
+		take = 5
+	}
+
+	hits, err := client.Hits.FindMany().OrderBy(
+		db.Hits.Hits.Order(db.DESC),
+	).Take(take).Exec(ctx)
+
+	if err != nil {
+		return c.Status(500).JSON(Response{
+			Success: false,
+			Message: "An internal server error occurred whilst fullfilling your request!",
+		})
+	}
 
 	return c.JSON(Response{
 		Success: true,
-		Message: "Here are the things",
+		Message: "Successfully retrieved hits leaderboard",
+		Data:    hits,
 	})
 }
