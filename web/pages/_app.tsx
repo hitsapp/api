5efcdec0ff83314cf00f89368094342ee51f98f0
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 17:20:16 -0400

    feat: global style

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,24 @@
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { fetcher, darkTheme } from "../utils";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background: ${({ theme }) => theme.layoutDark};
+    color: ${({ theme }) => theme.textLight};
+    font-family: 'Manrope', sans-serif;
+    font-weight: 500;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
       <SWRConfig
         value={{
           fetcher: fetcher,
