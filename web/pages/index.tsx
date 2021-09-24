1a4f313c98284f45d849ca78cf75384d10250fce
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 17:42:08 -0400

    styling

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from "next";
 import { Leaderboard, Nav } from "../components";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 const Home: NextPage = () => {
   return (
-    <>
+    <Container>
       <Nav />
       <Leaderboard />
-    </>
+    </Container>
   );
 };
 
