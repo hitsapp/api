f7aa1e7e8d454d91326e686a8c8c77aa8ae996b9
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 11:08:02 -0400

    feat: Leaderboard swr hook

--- web/hooks/useLeaderboard.ts
+++ web/hooks/useLeaderboard.ts
@@ -1,2 +1,15 @@
-import useSwr from "swr";
+import useSWR from "swr";
+import { Hit } from "../types";
+import { FULL_URI_V1 } from "../utils";
 
+export const useLeaderboard = () => {
+  console.log(`${FULL_URI_V1}/top`);
+  const { data, error } = useSWR(`${FULL_URI_V1}/top`);
+  const hits: Array<Hit> = data?.data;
+
+  return {
+    hits: hits,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
