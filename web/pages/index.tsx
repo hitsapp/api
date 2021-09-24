207334f3eebdb43eff1cd47f3058f2ef260a2a6b
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 11:08:18 -0400

    Feat: Show leaderboard on homepage

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import { Leaderboard } from "../components";
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>hey</h1>
+      <Leaderboard />
     </>
   );
 };
