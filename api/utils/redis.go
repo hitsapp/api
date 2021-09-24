b6a0ab7feadb987b3a055f06ae083f5529c23f66
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 17:14:33 -0400

    feat: redis utility

--- api/utils/redis.go
+++ api/utils/redis.go
@@ -1,25 +1,16 @@
 package utils
 
 import (
+	"os"
+
 	"github.com/gofiber/storage/redis"
 )
 
 func GetRedis() *redis.Storage {
-	store := redis.New()
+	var store = redis.New()
 
-	// Initialize custom config
 	store = redis.New(redis.Config{
-		Host:     "127.0.0.1",
-		Port:     6379,
-		Username: "",
-		Password: "",
-		Database: 0,
-		Reset:    false,
-	})
-
-	// or just the url with all information
-	store = redis.New(redis.Config{
-		URL:   "redis://localhost:6379",
+		URL:   os.Getenv("REDIS_URI"),
 		Reset: false,
 	})
 
