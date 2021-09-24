a6e7e8d40f8a6017e46c14fcdd298d04105af573
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 18:33:19 -0400

    a lot of styling

--- web/components/Leaderboard.tsx
+++ web/components/Leaderboard.tsx
@@ -1,29 +1,92 @@
 import { useLeaderboard } from "../hooks/useLeaderboard";
+import styled from "styled-components";
+import { Title } from ".";
+
+const Container = styled.div`
+  padding: 24px 24px;
+  background: ${({ theme }) => theme.layoutDark};
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+`;
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Place = styled.span`
+  min-width: 22px;
+  font-size: 1.2em;
+  text-align: right;
+`;
+
+const Hit = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 5px 0 5px 20px;
+`;
+
+const HitIcon = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.layoutLittleLessDark};
+  box-shadow: 0px 0px 12.1858px rgba(0, 0, 0, 0.25);
+`;
+
+const ListItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+`;
+
+const Link = styled.a`
+  margin: 3px 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.textDarkest};
+  transition: color 0.2s ease-in-out;
+
+  &:hover {
+    color: ${({ theme }) => theme.textLight};
+  }
+`;
 
 export const Leaderboard = () => {
   const { hits, isError, isLoading } = useLeaderboard();
 
   return (
-    <div>
-      <h1>Leaderboard</h1>
+    <Container>
+      <Title>Leaderboard</Title>
       {isLoading && <span>Loading hits</span>}
       {isError && <span>Error loading hits!</span>}
 
       {hits && hits.length > 0 ? (
-        <ul>
+        <List>
           {hits.map((hit, i) => {
             return (
-              <li key={i}>
-                {hit.hits}
-                <br />
-                {hit.url}
-              </li>
+              <ListItem key={i}>
+                <Place>{i + 1}</Place>
+                <Hit>
+                  <HitIcon></HitIcon>
+                  <ListItemInfo>
+                    <Link href={`https://${hit.url}/`}>https://{hit.url}/</Link>
+                    {hit.hits} hits
+                  </ListItemInfo>
+                </Hit>
+              </ListItem>
             );
           })}
-        </ul>
+        </List>
       ) : (
         <p>There are no hits yet!</p>
       )}
-    </div>
+    </Container>
   );
 };
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,18 +1,50 @@
 import type { NextPage } from "next";
-import { Leaderboard, Nav } from "../components";
+import { Leaderboard, Nav, Title } from "../components";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 80%;
+  height: 80vh;
   max-width: 1200px;
   margin: 0 auto;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 50px;
+`;
+
+const RightFlexContainers = styled.div`
+  margin: 10px 0;
+  padding: 24px 24px;
+  flex: 1 1;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.layoutDark};
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
 `;
 
 const Home: NextPage = () => {
   return (
     <Container>
       <Nav />
-      <Leaderboard />
+      <MainContent>
+        <Leaderboard />
+        <RightContainer>
+          <RightFlexContainers>
+            <Title>test</Title>
+          </RightFlexContainers>
+          <RightFlexContainers>
+            <Title>test</Title>
+          </RightFlexContainers>
+        </RightContainer>
+      </MainContent>
     </Container>
   );
 };
