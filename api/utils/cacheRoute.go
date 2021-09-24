6b1f0e1c59a479454caca728f54af721480a6be0
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-24 17:14:20 -0400

    Feat: Cache route utility

---
+++ api/utils/cacheRoute.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+)
+
+func CacheRoute() fiber.Handler {
+	return cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration:   10 * time.Minute,
+		CacheControl: true,
+		Storage:      GetRedis(),
+	})
+}
