15584213521e92fd0061494303bf4a10e3ea90cc
Bereket Semagn <semagnbereket@gmail.com>
2021-09-27 16:54:54 -0400

    check for NODE_ENV to specify BASE_URI

--- web/utils/consts.ts
+++ web/utils/consts.ts
@@ -1,2 +1,3 @@
-export const BASE_URI = "http://localhost";
+export const IS_PROD = process.env.NODE_ENV === "production";
+export const BASE_URI = IS_PROD ? "https://api.hits.link" : "http://localhost";
 export const API_URL = `${BASE_URI}/v1`;
