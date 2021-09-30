82ea050ea837a7a49f1bdaf560b8ab6c5c6f4900
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-29 20:59:27 -0400

    feat: favicons

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -14,6 +14,27 @@ const Page = () => {
       <Head>
         <meta charSet="UTF-8" />
         <title>Hits</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9E9E9E" />
+        <meta name="msapplication-TileColor" content="#9E9E9E" />
+        <meta name="theme-color" content="#9E9E9E" />
         <meta name="description" content="Count your hits!" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
