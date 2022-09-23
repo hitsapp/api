c27080c3fc4b1fe5c164d6f09bbcbbfb811bafc3
Cody Miller <hello@looskie.com>
2022-09-22 21:06:24 -0400

    :melt:

--- api/utils/redis_storage.go
+++ api/utils/redis_storage.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"time"
 
 	"github.com/gofiber/storage/redis"
 )
@@ -11,6 +12,10 @@ var redisdb *redis.Storage
 func SetRedis() {
 	if redisdb == nil {
 		println("REDIS URI", os.Getenv("REDIS_URI"))
+
+		println("waiting 150ms")
+		time.Sleep(150 * time.Millisecond)
+		println("waited 150ms, trying to connect now")
 
 		redisdb = redis.New(redis.Config{
 			URL:   os.Getenv("REDIS_URI"),
